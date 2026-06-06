b00d31a21fdbf55fe83487df00611f059c993ac3
Nikola Vujovic <nikolavujovickv@gmail.com>
2026-06-06 21:40:28 +0200

    Kreirana komponenta za prikaz kvizova

--- app/src/main/java/com/example/quizapp/ui/screens/home/components/QuizCard.kt
+++ app/src/main/java/com/example/quizapp/ui/screens/home/components/QuizCard.kt
@@ -1,9 +1,23 @@
 package com.example.quizapp.ui.screens.home.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun QuizCard(modifier: Modifier = Modifier) {
-    
+fun QuizCard(
+    modifier: Modifier = Modifier,
+    color: Color,
+    quizName: String) {
+
+    Card(
+        modifier = Modifier.fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(size = 24.dp)
+    ) { }
 }
