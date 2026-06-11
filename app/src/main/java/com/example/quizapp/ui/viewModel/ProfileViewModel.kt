fbde0fbf0a697b2b4bf54dbff0a0a3e7093e71ca
Nikola Vujovic <nikolavujovickv@gmail.com>
2026-06-11 15:28:00 +0200

    Kreiran ViewModel za Profile ekran

--- app/src/main/java/com/example/quizapp/ui/viewModel/ProfileViewModel.kt
+++ app/src/main/java/com/example/quizapp/ui/viewModel/ProfileViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.quizapp.ui.viewModel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.quizapp.data.model.User
 import com.example.quizapp.data.repository.ProfileRepository
 import com.example.quizapp.utils.Resource
@@ -8,17 +9,35 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
     private val profileRepository: ProfileRepository
 ): ViewModel(){
-    val _loggedUser = MutableStateFlow<Resource<User>>(Resource.Idle)
+    private val _loggedUser = MutableStateFlow<Resource<User>>(Resource.Idle)
     val loggedUser: StateFlow<Resource<User>> = _loggedUser.asStateFlow()
 
-    val _gamesPlayed = MutableStateFlow<Int>(0)
+    private val _gamesPlayed = MutableStateFlow<Int>(0)
     val gamesPlayed: StateFlow<Int> = _gamesPlayed.asStateFlow()
 
-    val _averageScore = MutableStateFlow<Int>(0)
+    private val _averageScore = MutableStateFlow<Int?>(0)
+    val averageScore: StateFlow<Int?> = _averageScore.asStateFlow()
+
+    private val _totalPoints = MutableStateFlow<Int?>(0)
+    val totalPoints: StateFlow<Int?> = _totalPoints.asStateFlow()
+
+    fun loadUser(userId: String){
+        viewModelScope.launch {
+            _loggedUser.value = Resource.Loading
+            _loggedUser.value = profileRepository.getLoggedUserInfo(userId)
+
+            val user = (_loggedUser.value as? Resource.Success)?.data
+            _averageScore.value = user?.accuracyPercentage
+            _gamesPlayed.value = user?.gamesPlayed ?: 0
+            _totalPoints.value = user?.totalPoints ?: 0
+        }
+    }
+
 }
